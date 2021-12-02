974d3832e55fc339e33d87ecf2412f1196aa1e15
anon1303 <jomari.daison@gmail.com>
2021-12-02 16:08:32 +0800

    fix some bugs

--- Dice.py
+++ Dice.py
@@ -1,58 +1,76 @@
 import random
 
-ans = "Y"
-while ans == "Y" or ans == "y":
-	
-	x = random.randint(1,6)
-	if x == 1:
-		print("************")
-		print("*          *")
-		print("*    O     *")
-		print("*          *")
-		print("************")
 
-	elif x == 2:
-		print("************")
-		print("*    O     *")
-		print("*          *")
-		print("*    O     *")
-		print("************")
 
-	elif x == 3:
-		print("************")
-		print("*   O      *")
-		print("*     O    *")
-		print("*       O  *")
-		print("************")
 
-	elif x == 4:
-		print("************")
-		print("*  O    O  *")
-		print("*          *")
-		print("*  O    O  *")
-		print("************")
+def dice(ans):
+	if ans == "y":
+		
+		x = random.randint(1,6)
+		if x == 1:
+			print("************")
+			print("*          *")
+			print("*    O     *")
+			print("*          *")
+			print("************")
 
-	elif x == 5:
-		print("************")
-		print("*  O    O  *")
-		print("*    O     *")
-		print("*  O    O  *")
-		print("************")
+		elif x == 2:
+			print("************")
+			print("*    O     *")
+			print("*          *")
+			print("*    O     *")
+			print("************")
 
-	elif x == 6:
-		print("************")
-		print("*  O    O  *")
-		print("*  O    O  *")
-		print("*  O    O  *")
-		print("************")
-	ans = input("Try again?, Y or N ")
+		elif x == 3:
+			print("************")
+			print("*   O      *")
+			print("*     O    *")
+			print("*       O  *")
+			print("************")
 
-	if ans == "N" or ans == "n":
+		elif x == 4:
+			print("************")
+			print("*  O    O  *")
+			print("*          *")
+			print("*  O    O  *")
+			print("************")
+
+		elif x == 5:
+			print("************")
+			print("*  O    O  *")
+			print("*    O     *")
+			print("*  O    O  *")
+			print("************")
+
+		elif x == 6:
+			print("************")
+			print("*  O    O  *")
+			print("*  O    O  *")
+			print("*  O    O  *")
+			print("************")
+
+		ans = input("Try again?, Y or N: \n").lower()
+
+		if ans == "n":
+			print("THANK YOU FOR PLAYING")
+
+		elif ans == "y":
+			dice(ans)	
+		else:
+			print("nnn")
+			print("Press Y to try again and N to stop!")
+			print("_"*35)
+			ans = input("Try again?, Y or N: \n").lower()
+			dice(ans)
+
+	elif ans == "n":
 		print("THANK YOU FOR PLAYING")
 
 	else:
 		print("Press Y to try again and N to stop!")
-		print("___________________________________")
-		ans = input("Try again?, Y or N ")
+		print("_"*35)
+		ans = input("Try again?, Y or N: \n").lower()
+		dice(ans)
 
-
+ans = "y"
+dice(ans)
